26bfb8c10d032d8819a695f465c6dd09874549fa
Lorena <lorena14-04@hotmail.com>
2016-12-04 01:12:31 +0100

    Añadida funcionalidad mostrar agenda de citas previstas en ese día de un médico al loguearse

---
+++ src/main/java/es/uvigo/esei/dagss/controladores/medico/AgendaControlador.java
@@ -0,0 +1,40 @@
+package es.uvigo.esei.dagss.controladores.medico;
+
+import es.uvigo.esei.dagss.dominio.daos.CitaDAO;
+import es.uvigo.esei.dagss.dominio.entidades.Cita;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class AgendaControlador implements Serializable {
+
+  private List<Cita> citas;
+
+  @Inject
+  CitaDAO citaDAO;
+
+  @Inject
+  MedicoControlador medicoControlador;
+
+  public AgendaControlador(){
+  }
+  @PostConstruct
+  public void inicializar() {
+    citas = citaDAO.buscarPorIdMedico(medicoControlador.getMedicoActual().getId());
+  }
+
+  public List<Cita> getCitas() {
+    return citas;
+  }
+
+  public void setCitas(List<Cita> citas) {
+    this.citas = citas;
+  }
+
+}
--- src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
+++ src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
@@ -5,13 +5,25 @@
 package es.uvigo.esei.dagss.dominio.daos;
 
 import es.uvigo.esei.dagss.dominio.entidades.Cita;
+import es.uvigo.esei.dagss.dominio.entidades.Medico;
+import es.uvigo.esei.dagss.dominio.entidades.Receta;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
+import java.util.Calendar;
+import java.util.List;
 
 
 @Stateless
 @LocalBean
-public class CitaDAO  extends GenericoDAO<Cita>{    
+public class CitaDAO  extends GenericoDAO<Cita>{
 
-    // Completar aqui
+  public List<Cita> buscarPorIdMedico(Long id) {
+    TypedQuery<Cita> q = em.createQuery("SELECT c FROM Cita c " +
+      "WHERE c.medico.id = :idMedico AND c.fecha=CURRENT_DATE", Cita.class);
+    q.setParameter("idMedico", id);
+    return q.getResultList();
+  }
+
 }
